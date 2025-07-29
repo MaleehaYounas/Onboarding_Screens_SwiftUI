67a1ab9eb50b4ed0cfd893efd401c8c69c3b652a
MaleehaYounas <maleeha139@gmail.com>
2025-07-29 17:54:15 +0500

    Implemented TabView

---
+++ OnboardingScreens/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  OnboardingScreens
+//
+//  Created by MAC on 29/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnboardingScreens/Model.swift
@@ -0,0 +1,18 @@
+//
+//  Model.swift
+//  OnboardingScreens
+//
+//  Created by MAC on 29/07/2025.
+//
+
+import SwiftUI
+
+struct Model: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Model()
+}
